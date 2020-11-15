13743733f7a2f10ecd4adfbf6865846517207d2b
brandonfranz13 <brandonfranz13@gmail.com>
2020-11-15 00:15:24 -0600

    astar bug fixes

--- scripts/planners/P1_astar.py
+++ scripts/planners/P1_astar.py
@@ -160,7 +160,7 @@ class AStar(object):
         """
         ########## Code starts here ##########
         while len(self.open_set) > 0:
-            if !self.is_free(self.x_goal):
+            if not self.is_free(self.x_goal):
                 return False
             current = self.find_best_est_cost_through()
             if current == self.x_goal:
